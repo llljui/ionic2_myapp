d7bf589401bf304875e2c6e60c20d73685d4d0fc
xijiang <872013164@qq.com>
2017-06-02 17:46:20 +0800

    app

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular'; 
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public loadingCtrl: LoadingController) {
 
   }
-
+presentLoading() { 
+        let loader = this.loadingCtrl.create({ 
+            content: "Please wait...", 
+            duration: 3000 
+        }); 
+        loader.present(); 
+    } 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { App, MenuController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,app: App, menu: MenuController,public loadingCtrl: LoadingController) {
+  menu.enable(true);
+  }
+    presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 1000
+    });
+    loader.present();
+  }
+    public event = {
+    month: '1990-02-19',
+    timeStarts: '07:43',
+    timeEnds: '1990-02-20'
   }
 
 }
